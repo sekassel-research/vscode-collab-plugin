ec6b2b3a9e4cfd5b9737ed75d04d703b1025a7f9
Pascal Drinkuth <uk070552@student.uni-kassel.de>
2023-01-01 23:30:37 +0100

    wsc | send and receive implemented

--- src/ws.ts
+++ src/ws.ts
@@ -4,6 +4,11 @@ const ws = new WebSocket('ws://localhost:8080');
 
 export function openWS(){
     ws.on('open', function open() {
+        console.log("open called")
         ws.send('Hello World');
       });
 }
+
+ws.on('message', function incoming(data:any) {
+    console.log(Buffer.from(data).toString());
+  });
