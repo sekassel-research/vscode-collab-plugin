e563a3c464012d563758bcc18a38d079b0e4bd5d
Pascal Drinkuth <uk070552@student.uni-kassel.de>
2023-04-07 23:44:25 +0200

    added logic for lineJump feature

--- media/main.js
+++ media/main.js
@@ -43,7 +43,6 @@
         }
     });
 
-    console.log("init chatView scripts");
     vscode.postMessage({type: 'initChat'});
 
     function addMsg(message) {
@@ -66,30 +65,51 @@
         const ul = document.querySelector('.chatBody');
         ul.textContent = '';
         for (const message of chat) {
-            const chatMsg = document.createElement('div');
-            chatMsg.className = 'chatMsg';
-            chatMsg.title = new Date(message.time).toLocaleString('de-DE');
-            const user = document.createElement('user');
-            user.className = 'userName';
-            user.appendChild(document.createTextNode(message.name));
-            if (message.name === userName) {
-                chatMsg.style.border = "1px solid #1139EE";
-                chatMsg.style.backgroundColor = "#4169E1";
-            } else {
-                chatMsg.style.border = "1px solid lightblue";
-                chatMsg.style.backgroundColor = "#3B494F";
-            }
-
-            const content = document.createElement('content');
-            content.className = 'content';
-            content.appendChild(document.createTextNode(message.msg));
-
-            chatMsg.appendChild(user);
-            chatMsg.appendChild(content);
-
-            ul.appendChild(chatMsg);
+          const chatMsg = document.createElement('div');
+          chatMsg.className = 'chatMsg';
+      
+          // RegEx-Ausdruck zur Extraktion der Zeilennummer und des "Line"/"Zeile"-Präfixes
+          const regex = /(Line|Zeile)\s*(\d+)/gi;
+          const matches = message.msg.matchAll(regex);
+      
+          // Erstellen des Titels mit allen Zeilennummern
+          const lineNumberStr = Array.from(matches, match => `Line ${match[2]}`).join(', ');
+          chatMsg.title = new Date(message.time).toLocaleString('de-DE');
+      
+          const user = document.createElement('user');
+          user.className = 'userName';
+          user.appendChild(document.createTextNode(message.name));
+          if (message.name === userName) {
+            chatMsg.style.border = "1px solid #1139EE";
+            chatMsg.style.backgroundColor = "#4169E1";
+          } else {
+            chatMsg.style.border = "1px solid lightblue";
+            chatMsg.style.backgroundColor = "#3B494F";
+          }
+      
+          const content = document.createElement('content');
+          content.className = 'content';
+      
+          // Ersetzen von "Line" oder "Zeile" gefolgt von Leerzeichen und Ziffern durch Links
+          const msgWithLinks = message.msg.replace(regex, '<a href="#" class="lineLink">$&</a>');
+          content.innerHTML = msgWithLinks;
+      
+          chatMsg.appendChild(user);
+          chatMsg.appendChild(content);
+      
+          ul.appendChild(chatMsg);
         }
-    }
+      
+        // Hinzufügen eines Event-Listeners für alle Links mit der Klasse "lineLink"
+        const lineLinks = document.querySelectorAll('.lineLink');
+        lineLinks.forEach(link => {
+          link.addEventListener('click', e => {
+            e.preventDefault();
+            const lineNumber = e.target.textContent.match(/\d+/)[0];
+            console.log(lineNumber);
+          });
+        });
+      }
 
     function updateStyle() {
         msgInput.style.height = 'auto';
