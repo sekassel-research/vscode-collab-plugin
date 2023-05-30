b99c69ea27077758eefb6ada4aac6aa0bd4a348d
Pascal Drinkuth <uk070552@student.uni-kassel.de>
2023-05-30 23:10:39 +0200

    ...forgot to safe jsonUtils

--- plugin/src/util/jsonUtils.ts
+++ plugin/src/util/jsonUtils.ts
@@ -1,21 +1,21 @@
 import * as vscode from 'vscode';
 
 export function buildUserMessage(operation: string, userId: string, project: string) {
-    return JSON.stringify({operation, data: {name: userId, project}});
+    return JSON.stringify({operation, data: {userId: userId, project}});
 }
 
-export function buildCursorMovedMessage(operation: string, pathName: string, cursor: vscode.Position, selectionEnd: vscode.Position, name: string, project: string) {
-    return JSON.stringify({operation, data: {pathName, cursor, selectionEnd, name, project}});
+export function buildCursorMovedMessage(operation: string, pathName: string, cursor: vscode.Position, selectionEnd: vscode.Position, userId: string, project: string) {
+    return JSON.stringify({operation, data: {pathName, cursor, selectionEnd, userId, project}});
 }
 
-export function buildSendTextReplacedMessage(operation: string, pathName: string, from: vscode.Position, to: vscode.Position, content: string, name: string, project: string) {
-    return JSON.stringify({operation, data: {pathName, from, to, content, name, project}});
+export function buildSendTextReplacedMessage(operation: string, pathName: string, from: vscode.Position, to: vscode.Position, content: string, userId: string, project: string) {
+    return JSON.stringify({operation, data: {pathName, from, to, content, userId, project}});
 }
 
-export function buildChatMessage(operation: string, msg: string, name: string, project: string) {
-    return JSON.stringify({operation, data: {msg, name, time: new Date(), project}});
+export function buildChatMessage(operation: string, msg: string, userId: string, project: string) {
+    return JSON.stringify({operation, data: {msg, userId, time: new Date(), project}});
 }
 
-export function buildSendTextDelKeyMessage(operation: string, pathName: string, from: vscode.Position, delLinesCounter: number, delCharCounter: number, name: string, project: string) {
-    return JSON.stringify({operation, data: {pathName, from, delLinesCounter, delCharCounter, name, project}});
+export function buildSendTextDelKeyMessage(operation: string, pathName: string, from: vscode.Position, delLinesCounter: number, delCharCounter: number, userId: string, project: string) {
+    return JSON.stringify({operation, data: {pathName, from, delLinesCounter, delCharCounter, userId, project}});
 }
